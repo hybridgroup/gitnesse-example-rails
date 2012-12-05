9f217245fa05f26a76372bd044bcc9b0f176ce92
Andrew Stewart <andrew@averagestudios.com>
2012-12-05 11:43:32 -0800

    Updated gitnesse version and configuration

--- config/gitnesse.rb
+++ config/gitnesse.rb
@@ -1,3 +1,5 @@
-Gitnesse.config do
-  repository_url "https://github.com/hybridgroup/gitnesse-demo.wiki.git"
+Gitnesse.configure do |config|
+  config.repository_url   = "https://github.com/hybridgroup/gitnesse-demo.wiki.git"
+  config.annotate_results = true
+  config.info             = "Development laptop"
 end
